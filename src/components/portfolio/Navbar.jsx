b06c3f26264e14bb2b96257ddb86ffe6590ae661
Healer404 <dukaroniel0701@gmail.com>
2022-11-11 19:09:22 +0800

    added the navbar

--- src/components/portfolio/Navbar.jsx
+++ src/components/portfolio/Navbar.jsx
@@ -1,30 +1,38 @@
 import { Component } from "react";
+import ReactVisibilitySensor from "react-visibility-sensor";
 
 class Navbar extends Component{
+  state = {
+    visibility: false,
+  }
   render(){
     return (
-      <nav className="PRTFnavbar active">
-      <a href="#introduction" className="item">
-        <i className="fas fa-file"></i>
-        <span>Resume</span>
-      </a>
-      <a href="#about" className="item">
-        <i className="fas fa-user"></i> 
-        <span>About Me</span>
-      </a>
-      <a href="/" className="item brand">
-        <img src="./assets/happs.png" alt="happs" />
-        <span>Home</span>
-      </a>
-      <a href="#contact" className="item">
-        <i className="fas fa-phone"></i>
-        <span>Contact</span>
-      </a>
-      <a href="#projects" className="item">
-        <i className="fas fa-folder"></i>
-        <span>Projects</span>
-      </a>
-    </nav>
+      <ReactVisibilitySensor onChange={(isVisible) => {
+        this.setState({visibility: isVisible});
+      }}>
+        <nav className={this.state.visibility ? "PRTFnavbar active" : "PRTFnavbar"}>
+          <a href="#introduction" className="item">
+            <i className="fas fa-file"></i>
+            <span>Resume</span>
+          </a>
+          <a href="#about" className="item">
+            <i className="fas fa-user"></i> 
+            <span>About Me</span>
+          </a>
+          <a href="/" className="item brand">
+            <img src="./assets/happs.png" alt="happs" />
+            <span>Home</span>
+          </a>
+          <a href="#contact" className="item">
+            <i className="fas fa-phone"></i>
+            <span>Contact</span>
+          </a>
+          <a href="#projects" className="item">
+            <i className="fas fa-folder"></i>
+            <span>Projects</span>
+          </a>
+        </nav>
+      </ReactVisibilitySensor>
     )
   }
 }
--- src/page/portfolio/Portfolio.jsx
+++ src/page/portfolio/Portfolio.jsx
@@ -2,11 +2,13 @@ import { Component } from "react";
 import './portfolio.css';
 
 import Intro from "../../components/Intro";
+import Navbar from "../../components/portfolio/Navbar";
 class PortfolioPage extends Component{
   render(){
     return (
       <>
         <Intro />
+        <Navbar />
       </>
     )
   }
