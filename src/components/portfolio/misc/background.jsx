a3a52cd6d3a6395597666f842d17069dafba63d0
Healer404 <dukaroniel0701@gmail.com>
2022-11-13 16:40:21 +0800

    added background with animation

---
+++ src/components/portfolio/misc/background.jsx
@@ -0,0 +1,21 @@
+import './background.css';
+
+const BackgroundAnimation = () => {
+  return (
+    <div className="background-area">
+      <ul className="boxed">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  )
+}
+export default BackgroundAnimation;
--- src/page/portfolio/Portfolio.jsx
+++ src/page/portfolio/Portfolio.jsx
@@ -2,9 +2,12 @@ import './portfolio.css';
 import Introduction from '../../components/portfolio/introduction/introduction';
 import AboutMe from '../../components/portfolio/about/about';
 
+import BackgroundAnimation from '../../components/portfolio/misc/background';
+
 const PortfolioPage = () => {
   return (
     <>
+      <BackgroundAnimation />
       <Introduction />
       <AboutMe />
     </>
