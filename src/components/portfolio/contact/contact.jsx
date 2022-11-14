5c3dd52f1075c77ada9ce8aab5988a3084b89fb0
Healer404 <dukaroniel0701@gmail.com>
2022-11-14 20:26:16 +0800

    added and modified the contact and footer

--- src/components/portfolio/contact/contact.jsx
+++ src/components/portfolio/contact/contact.jsx
@@ -1,22 +1,18 @@
 import { useRef } from 'react';
 import './contact.css';
-import SectionHeader from '../misc/header';
 import Observer from '../../../hooks/observer';
-import ContactItem from './contactItem';
 
 const Contact = () => {
-  const refContactMe = useRef(null);
-  const headerDetails= {
-    title: "Contact Me",
-    subtitle: "Let's create something",
-    ref: Observer(refContactMe),
-    id: "contact"
-  };
+  const refContact = useRef(null);
+  const refEmail = useRef(null);
+  const refSocial = useRef(null);
+  const refNavigation = useRef(null);
+  const refCV = useRef(null);
   const contactDetails = [
     {
-      link: 'mailto:ronielduka@gmail.com',
-      icon: 'fas fa-envelope',
-      label: 'Email'
+      link: 'viber://chat?number=639677153243',
+      icon: 'fab fa-viber',
+      label: 'Viber'
     },
     {
       link: 'viber://chat?number=639677153243',
@@ -35,15 +31,47 @@ const Contact = () => {
     }
   ];
   return (
-    <section className="PRTFsection" ref={refContactMe}>
-      <SectionHeader header={headerDetails} />
-      <main className={`contactContainer ${Observer(refContactMe) ? "active" : ""}`}>
-        <div className="contactOptions">
-          <h2>Message me on</h2>
-          {contactDetails.map((contactItem) => (
-            <ContactItem details={contactItem} />
-          ))};
+    <section className="PRTFsection">
+      <main className="contact-container ">
+        <header className={`contact-texts ${Observer(refContact) ? "active" : ""}`} ref={refContact}>
+          <h2>Get in touch</h2>
+          <span>Let's create something new!</span>
+        </header>
+
+        <div className="contact-links">
+          <div className={`links ${Observer(refEmail) ? "active" : ""}`} ref={refEmail}>
+            <h4>Email</h4>
+            <a href="mailto:ronielduka@gmail.com">
+              ronielduka@gmail.com
+            </a>
+          </div>
+          
+          <div className={`links ${Observer(refSocial) ? "active" : ""}`} ref={refSocial}>
+            <h4>Social</h4>
+            {contactDetails.map((contactLink, index) => (
+              <a href={contactLink.link} key={index} target="_blank" rel="noreferrer">
+                <i className={contactLink.icon}></i>
+                {contactLink.label}
+              </a>
+            ))};
+          </div>
+            
+          <div className={`links ${Observer(refNavigation) ? "active" : ""}`} ref={refNavigation}>
+            <h4>Navigation</h4>
+            <a href="#introduction">Home</a>
+            <a href='#about'>About Me</a>
+            <a href="#projects">Projects</a>
+          </div>
+
+          <div className={`links ${Observer(refCV) ? "active" : ""}`} ref={refCV}>
+            <h4>Resume</h4>
+            <a href='#introduction' className='btn-secondary cv-button' target="_blank" rel='noreferrer'>Download CV</a>
+          </div>
         </div>
+
+        <footer className="footer">
+          <span>&copy; 2022 Roniel Duka</span>
+        </footer>
       </main>
     </section>
   )
